ec71911c441c6abbc8f5d076ea0f905ab228ecac
jeromecox <jeromecox@hotmail.com>
2023-02-22 17:06:33 -0600

    user registration endpoint

--- congregate/urls.py
+++ congregate/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.testview, name='test'),
     path('dj-rest-auth/google/', views.GoogleLogin.as_view(), name='google_login'),
+    path('register', views.new_user, name='registration'),
     path('<slug:username>/home/', views.UserHome.as_view(), name='home'),
 ]
+
+
+# /new/event
+# /new/option
--- congregate/views.py
+++ congregate/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from django.shortcuts import redirect, render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
@@ -6,14 +8,46 @@ from .models import CongregateUser
 # Group, Event, EventOption
 from .serializers import CongregateUserSerializer
 # EventSerializer, EventOptionSerializer
+from rest_framework import response
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 # ListCreateAPIView, ListAPIView, DestroyAPIView, UpdateAPIView
+import json
 
 # Create your views here.
 
 
 def testview(request):
     return render(request, 'test.html', {})
+
+
+@csrf_exempt
+def new_user(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data.get('username', None)
+        first_name = data.get('first_name', None)
+        last_name = data.get('last_name', None)
+        email = data.get('email', None)
+
+        # Validate required fields are present
+        if not all([username, first_name, last_name, email]):
+            return JsonResponse({'error': 'username, first_name, last_name, and email are required fields'}, status=400)
+
+        if CongregateUser.objects.filter(email=email).exists():
+            return JsonResponse({'error': 'user is already registered with this email'}, status=409)
+
+        # Check if the user already exists
+        if CongregateUser.objects.filter(username=username).exists():
+            return JsonResponse({'error': 'user with this username already exists'}, status=409)
+
+        # Create and save the new user
+        user = CongregateUser.objects.create(username=username, first_name=first_name, last_name=last_name, email=email)
+        user.save()
+
+        return redirect(f'/{user.username}/home/', status=201)
+
+    else:
+        return JsonResponse({'error': 'invalid request method'}, status=405)
 
 
 class GoogleLogin(SocialLoginView):
