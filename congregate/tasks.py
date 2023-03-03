26818304f923ecfc309115570adb893cbda167c6
jeromecox <jeromecox@hotmail.com>
2023-03-03 16:57:48 -0600

    activity winner declared when vote closing time met

--- congregate/tasks.py
+++ congregate/tasks.py
@@ -10,7 +10,7 @@ def decide_event(event_id):
     event = Event.objects.get(id=event_id)
 
     if not event.decided and timezone.now() >= event.vote_closing_time:
-        event_acts = Activity.objects.filter(event=event).annotate(total_votes=Sum('votes__vote')).order_by('total_votes')
+        event_acts = Activity.objects.filter(event=event).annotate(total_votes=Sum('votes__vote')).order_by('-total_votes')
 
         winning_activity = event_acts[0]
         winning_activity.is_winner = True
