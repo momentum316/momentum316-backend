e462a152ace8b5d296569bd49a46c03e4d1ff4f2
jeromecox <jeromecox@hotmail.com>
2023-03-03 19:08:52 -0600

    celery certs again

--- config/celery.py
+++ config/celery.py
@@ -5,17 +5,19 @@ from celery import Celery
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-# app = Celery(
-#     "config",
-#     broker_use_ssl={
-#         'ssl_cert_reqs': ssl.CERT_NONE
-#     },
-#     redis_backend_use_ssl={
-#         'ssl_cert_reqs': ssl.CERT_NONE
-#     }
-# )
+# for production
+app = Celery(
+    "config",
+    broker_use_ssl={
+        'ssl_cert_reqs': ssl.CERT_NONE
+    },
+    redis_backend_use_ssl={
+        'ssl_cert_reqs': ssl.CERT_NONE
+    }
+)
 
-app = Celery('config')
+# for development
+# app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
