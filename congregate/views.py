71de83663ac974387c6074ca9e963229c15940a3
jeromecox <jeromecox@hotmail.com>
2023-03-06 15:46:32 -0600

    work on logout view

--- congregate/views.py
+++ congregate/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import Sum
 from django.http import JsonResponse
 from django.shortcuts import redirect, get_object_or_404
